14b4896eca42f47743d5ed054612a5f35dd1c5c0
Jeremy Huang <huangzz@umich.edu>
2016-10-20 22:57:24 -0400

    new

--- Matlab_support/zhi_velo2img.m
+++ Matlab_support/zhi_velo2img.m
@@ -3,14 +3,21 @@ for i=1:297
     a = floor(velo);
     b = floor(velo*100);
     c = floor(velo*10000);
-    A = a;
-    A(:,:,2) = b - a*100;
-    A(:,:,3) = c - b*100;
+    c = c - b*100;
+    b = b - a*100;
+    A = uint8(a);
+    A(:,:,2) = uint8(b);
+    A(:,:,3) = uint8(c);
     
-    outputString = sprintf('./depthRGB/%d.png', i-1)
-    imwrite(A,outputString,'png');
+    outputString = sprintf('../data/depthRGB/%d.png', i-1)
+%     A(1,1,:)
+%     imwrite(A,outputString,'png');
+    imwrite(A,outputString);
 end
 
 
 %%
-b = imread('velo.png');
+b = imread('../data/depthRGB/0.png');
+depth = double(b(1,1,1)) + double(double(b(1,1,2))/100) + double(double(b(1,1,3)))/10000
+
+c = imread('../data/png16depth/1.png');
